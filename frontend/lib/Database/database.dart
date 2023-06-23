8c497930fe7bac2c05aa3a8ef568f23fd1671e9b
Sahilpervez <97945916+Sahilpervez@users.noreply.github.com>
2023-06-23 17:17:53 +0530

    Complete flutter app

--- frontend/lib/Database/database.dart
+++ frontend/lib/Database/database.dart
@@ -1,21 +1,25 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
 
 
 class Data extends ChangeNotifier{
 
-  final address = "http://10.102.141.76:3000";
+  final address = String.fromEnvironment("ADDRESS");
   Future<String> sendData(String id) async {
-    print("before sending request $id");
+    if (kDebugMode) {
+      print("before sending request $id");
+    }
     // final url = Uri.parse("$address/scan/").replace(queryParameters: {"id": id});
     final url = Uri.parse("$address/scan/$id");
-    print(url);
+    if (kDebugMode) {
+      print(url);
+    }
     final response = await http.post(url);
-    print("response: \n ${response.body}");
-    print("response type: \n ${response.body.runtimeType}");
+    if (kDebugMode) {
+      print("response: \n ${response.body}");
+      print("response type: \n ${response.body.runtimeType}");
+    }
     return response.body;
   }
 
--- frontend/lib/Screens/home_screen.dart
+++ frontend/lib/Screens/home_screen.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../Database/database.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,9 +15,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final qrKey = GlobalKey(debugLabel:  "QR");
+  final qrKey = GlobalKey(debugLabel: "QR");
   QRViewController? QRcontroller;
-  Barcode? code ;
+  Barcode? code;
   String? res;
 
   @override
@@ -33,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void reassemble() async {
     // TODO: implement reassemble
     super.reassemble();
-    if(Platform.isAndroid){
+    if (Platform.isAndroid) {
       await QRcontroller!.pauseCamera();
     }
     QRcontroller!.resumeCamera();
@@ -44,82 +43,207 @@ class _HomeScreenState extends State<HomeScreen> {
     final urlToPost = Provider.of<Data>(context);
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color(0xff252525),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            buildQRView(context),
-            Positioned(bottom: 10, child: buildResult(),),
-            Positioned(bottom: 50, child: ElevatedButton(
-              onPressed: (){
-                print("${code!.code}");
-                urlToPost.sendData(code!.code.toString()).then((value){
-                  print(value);
-                  if(value == "1"){
-                    res = "1";
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: Text("Success"),
-                          // content: Image(image: ),
-                          actions: [],
-                        ));
-                  }
-                  else if(value == "0"){
-                    res ="0";
-                  }
-                  else{
-                    res = "3";
-                  }
-                });
-              },
-              child: Text("Scan"),
-            ),),
-          ]
-        ),
+        backgroundColor: const Color(0xff252525),
+        body: Stack(alignment: Alignment.center, children: [
+          buildQRView(context),
+          Positioned(
+            bottom: 10,
+            child: buildResult(),
+          ),
+          Positioned(
+            bottom: 50,
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      QRcontroller?.stopCamera();
+                      QRcontroller?.resumeCamera();
+                    });
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.refresh,
+                          size: 35,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          "Refresh",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (kDebugMode) {
+                      print("${code!.code}");
+                    }
+                    QRcontroller?.pauseCamera();
+                    urlToPost.sendData(code!.code.toString()).then((value) {
+                      if (kDebugMode) {
+                        print(value);
+                      }
+                      if (value == "Scanned") {
+                        res = "Scanned";
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            title: const Text(
+                              "Success",
+                            ),
+                            content: const Image(
+                              image: AssetImage("Assets/success.gif"),
+                            ),
+                            actions: [
+                              TextButton.icon(
+                                icon: const Icon(
+                                  Icons.done,
+                                  color: Colors.green,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                label: const Text(
+                                  "Done",
+                                  style: TextStyle(
+                                      color: Colors.green, fontSize: 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (value == "Already scanned") {
+                        res = "Already scanned";
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            title: const Text(
+                              "Already Scanned!!",
+                            ),
+                            content: const Image(
+                              image: AssetImage("Assets/already_scanned.gif"),
+                            ),
+                            actions: [
+                              TextButton.icon(
+                                icon: const Icon(
+                                  Icons.close,
+                                  color: Colors.red,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                  setState(() {});
+                                },
+                                label: const Text(
+                                  "Close",
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (value == "Not found") {
+                        res = "Not found";
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            title: const Text(
+                              "Coupon not Found!!",
+                            ),
+                            content: const Image(
+                              image: AssetImage("Assets/not_found.gif"),
+                            ),
+                            actions: [
+                              TextButton.icon(
+                                icon: const Icon(
+                                  Icons.close,
+                                  color: Colors.red,
+                                  size: 25,
+                                ),
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                label: const Text(
+                                  "Close",
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    });
+                    QRcontroller?.resumeCamera();
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.qr_code_scanner_rounded,
+                          size: 35,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          "Scan",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
 
-  Widget buildResult(){
+  Widget buildResult() {
     return Container(
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 5),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          color : Colors.white54,
+          color: Colors.white54,
         ),
-        child: Text((code!= null) ? "Result : ${code!.code}" : "Scan a code"));
+        child: Text((code != null) ? "Result : ${code!.code}" : "Scan a code"));
   }
 
-  Widget buildQRView(BuildContext context){
+  Widget buildQRView(BuildContext context) {
     return QRView(
       key: qrKey,
       onQRViewCreated: onQRViewCreated,
       overlay: QrScannerOverlayShape(
-        borderColor: Color(0xff7CFFB8),
+        borderColor: const Color(0xff7CFFB8),
         borderWidth: 10,
         borderLength: 30,
         borderRadius: 20,
-        cutOutSize: MediaQuery.of(context).size.width *0.8,
+        cutOutSize: MediaQuery.of(context).size.width * 0.8,
       ),
     );
   }
 
-  void onQRViewCreated(QRcontroller){
+  void onQRViewCreated(QRcontroller) {
     setState(() => this.QRcontroller = QRcontroller);
-    QRcontroller!.scannedDataStream.listen((code) => setState(() => this.code = code));
+    QRcontroller!.scannedDataStream
+        .listen((code) => setState(() => this.code = code));
   }
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Color(0xff252525),
-  //     body: Center(
-  //       child: ElevatedButton(
-  //         onPressed: (){
-  //           _qrScanner();
-  //         },
-  //         child: Text("Scan"),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
